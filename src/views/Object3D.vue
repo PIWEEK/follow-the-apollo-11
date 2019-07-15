b3a5b3466cc5c253ba998daed079500f127445e0
maria.cano <maria.cano@kaleidos.net>
2019-07-15 17:00:16 +0200

    add gftl model

---
+++ src/views/Object3D.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="object3D">
+    <a-scene background="color: #ECECEC">
+      <a-assets>
+        <a-asset-item id="cube" src="./assets/cube.gltf"></a-asset-item>
+      </a-assets>
+      <a-entity gltf-model="#cube"></a-entity>
+    </a-scene>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'object3D'
+}
+</script>
