59ea66eb722f3457d4b112ecf884cf92224db237
maria.cano <maria.cano@kaleidos.net>
2019-07-15 11:51:26 +0200

    basic ar config

---
+++ src/views/Ar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ar">
+    <p>AR</p>
+    <a-scene embedded arjs>
+      <a-marker preset="hiro">
+          <a-box position='0 0.5 0' material='color: yellow;'></a-box>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ar'
+}
+</script>
