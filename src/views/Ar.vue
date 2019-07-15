548ef98faad6186b65f4698cd0f1ce0a08616b10
David Barragán Merino <david.barragan@kaleidos.net>
2019-07-15 20:18:49 +0200

    Add some sample markers

--- src/views/Ar.vue
+++ src/views/Ar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ar">
     <p>AR</p>
-    <a-scene arjs='sourceType: webcam;'>
-      <a-box position='0 0.5 0' material='opacity: 0.5; color: yellow;'></a-box>
-      <a-marker-camera preset='hiro'></a-marker-camera>
+    <a-scene arjs='sourceType: webcam;' v-pre>
+      <a-box position='0 0.5 0' material='opacity: 0.5; color: yellow;' v-pre></a-box>
+      <a-marker-camera type='pattern' url='./img/markers/cobete.patt' v-pre></a-marker-camera>
     </a-scene>
   </div>
 </template>
