568d208ef7a1c86b24322142dfea9641a78d9429
maria.cano <maria.cano@kaleidos.net>
2019-07-18 16:34:59 +0200

    add animations

--- src/views/Object3D.vue
+++ src/views/Object3D.vue
@@ -6,13 +6,23 @@
       <a-assets>
         <a-asset-item id="rocket" src="img/models/rocket.gltf" >
         </a-asset-item>
+        <a-asset-item id="rocket_animate" src="img/models/rocket_animate.gltf" >
+        </a-asset-item>
         <a-asset-item id="eagle" src="img/models/eagle.gltf" >
         </a-asset-item>
-        <a-asset-item id="helmet" src="img/models/helmet_animate.gltf" >
+        <a-asset-item id="eagle_animate" src="img/models/eagle_animate.gltf" >
+        </a-asset-item>
+        <a-asset-item id="helmet" src="img/models/helmet.gltf" >
+        </a-asset-item>
+        <a-asset-item id="helmet_animate" src="img/models/helmet_animate.gltf" >
         </a-asset-item>
         <a-asset-item id="trajectory" src="img/models/trajectory.gltf" >
         </a-asset-item>
+        <a-asset-item id="trajectory_animate" src="img/models/trajectory_animate.gltf" >
+        </a-asset-item>
         <a-asset-item id="map" src="img/models/map.gltf" >
+        </a-asset-item>
+        <a-asset-item id="map_animate" src="img/models/map_animate.gltf" >
         </a-asset-item>
       </a-assets>
 
@@ -24,6 +34,15 @@
           position="0 1 0"></a-gltf-model>
       </a-marker>
 
+      <a-gltf-model
+        src="#rocket_animate"
+        rotation="0 0 0"
+        scale="1 1 1"
+        position="0 1 -10"
+        animation-mixer
+        >
+      </a-gltf-model>
+
       <a-marker type='pattern' url='img/markers/eagle.patt' v-pre>
         <a-gltf-model
           src="#eagle"
@@ -32,16 +51,32 @@
           scale="1 1 1"></a-gltf-model>
       </a-marker>
 
+      <a-gltf-model
+        src="#eagle_animate"
+        rotation="0 0 0"
+        scale="1 1 1"
+        position="0 1 -10"
+        animation-mixer
+        >
+      </a-gltf-model>
+
       <a-marker type='pattern' url='img/markers/helmet.patt' v-pre>
         <a-gltf-model
           src="#helmet"
-          rotation="-90 0 0"
+         rotation="-90 0 0"
           scale="1 1 1"
-          position="0 1 0"
-          animation_mixer="clip: *;"
-          >
+          position="0 1 0">
         </a-gltf-model>
       </a-marker>
+
+      <a-gltf-model
+        src="#helmet_animate"
+        rotation="0 0 0"
+        scale="1 1 1"
+        position="0 1 -10"
+        animation-mixer
+        >
+      </a-gltf-model>
 
       <a-marker type='pattern' url='img/markers/trajectory.patt' v-pre>
         <a-gltf-model
@@ -53,6 +88,15 @@
         </a-gltf-model>
       </a-marker>
 
+      <a-gltf-model
+        src="#trajectory_animate"
+        rotation="0 0 0"
+        scale="1 1 1"
+        position="0 1 -10"
+        animation-mixer
+        >
+      </a-gltf-model>
+
       <a-marker type='pattern' url='img/markers/map.patt' v-pre>
         <a-gltf-model
           src="#map"
@@ -62,6 +106,15 @@
           >
         </a-gltf-model>
       </a-marker>
+
+      <a-gltf-model
+        src="#map_animate"
+        rotation="0 0 0"
+        scale="1 1 1"
+        position="0 1 -10"
+        animation-mixer
+        >
+      </a-gltf-model>
 
       <a-entity light="type: ambient; color: #ccc;"></a-entity>
       <a-entity light="type: directional; color: #ccc; intensity: 2.5;" position="0 14 0"></a-entity>
